95ec4847bad1c3005f172227db486ee50240dfef
Javier Gonzalez <javierggt@yahoo.com>
2020-01-29 14:43:12 -0500

    Remove dependencies on any other ska3 package. This should be lightweight.

--- skare3_tools/__init__.py
+++ skare3_tools/__init__.py
@@ -1,4 +1,17 @@
+from pkg_resources import get_distribution, DistributionNotFound
+from setuptools_scm import get_version
+import warnings
 
-import ska_helpers
 
-__version__ = ska_helpers.get_version(__package__)
+try:
+    try:
+        _dist_info = get_distribution(__name__)
+        __version__ = _dist_info.version
+        # the following fails with a local repo with an egg in it
+        assert __file__.lower().startswith(_dist_info.location.lower())
+    except (DistributionNotFound, AssertionError):
+        # this might be a local git repo
+        __version__ = get_version(root='..', relative_to=__file__)
+except Exception:
+    warnings.warn('Failed to find skare3_tools package version, setting to 0.0.0')
+    __version__ = '0.0.0'
