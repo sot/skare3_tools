3210598d91dab07b7ebba2a247f78d9a38c7cb19
Javier Gonzalez <javierggt@yahoo.com>
2020-05-15 09:20:17 -0400

    added script for milestone-related issues

---
+++ skare3_tools/github/scripts/milestone_issues.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+"""
+Get milestone-related issues from a Github repository .
+"""
+
+import argparse
+import jinja2
+from skare3_tools.github import graphql as github
+
+
+def milestone_issues(milestone):
+    query = jinja2.Template(github.REPO_ISSUES_QUERY).render(
+        owner='sot', name='skare3', label='"Package update"')
+    info = github.GITHUB_API(query)['data']['repository']['issues']
+    nodes = sorted(info['nodes'], key=lambda n: n['title'])
+    nodes = [issue for issue in nodes
+             if issue['milestone'] and issue['milestone']['title'] == milestone]
+    return nodes
+
+
+def get_parser():
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('--repository', default='sot/skare3')
+    parser.add_argument('--milestone')
+    return parser
+
+
+def main():
+    args = get_parser().parse_args()
+    github.init()
+    issues = milestone_issues(args.milestone)
+    for issue in issues:
+        print(f'Fixes #{issue["number"]}')
+
+
+if __name__ == '__main__':
+    main()
