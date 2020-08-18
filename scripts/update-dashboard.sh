634182537493ba630ad6dd8b32611b2b31ac77ad
Javier Gonzalez <javierggt@yahoo.com>
2020-08-17 20:42:51 -0400

    horrible hack

--- scripts/update-dashboard.sh
+++ scripts/update-dashboard.sh
@@ -1,9 +1,12 @@
 source $HOME/.ci-auth
 skare3-dashboard -o /proj/sot/ska/jgonzalez/index.html
-skare3-test-dashboard -o /proj/sot/ska/jgonzalez/test_results.html
+skare3-test-dashboard -o /proj/sot/ska/jgonzalez/test_results.html --static-dir https://cxc.cfa.harvard.edu/mta/ASPECT/skare3/dashboard/static --log-dir https://icxc.cfa.harvard.edu/aspect/skare3/dashboard/tests
 
-mv index.html /proj/sot/ska/www/ASPECT/skare3/dashboard/
-mv /proj/sot/ska/www/ASPECT_ICXC/skare3/testr/logs/last/test_results.html /proj/sot/ska/www/ASPECT/skare3/dashboard/tests/index.html
+# horrible hack:
+rm -f /proj/sot/ska/www/ASPECT_ICXC/skare3/dashboard/tests
+ln -s `/proj/sot/ska/jgonzalez/git/skare3_tools/scripts/test_dir.py` /proj/sot/ska/www/ASPECT_ICXC/skare3/dashboard/tests
+mv /proj/sot/ska/jgonzalez/index.html /proj/sot/ska/www/ASPECT/skare3/dashboard/
+mv /proj/sot/ska/jgonzalez/test_results.html /proj/sot/ska/www/ASPECT/skare3/dashboard/tests/index.html
 
 # echo files copied to /proj/sot/ska/www/ASPECT/skare3/dashboard
 # echo dashboard at https://cxc.cfa.harvard.edu/mta/ASPECT/skare3/dashboard/
