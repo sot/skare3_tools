d629b8d87f2b288ba5736db0a60dd6e151a6112c
Javier Gonzalez <javierggt@yahoo.com>
2020-10-23 18:22:27 -0400

    revert previous

--- skare3_tools/scripts/skare3_release_check.py
+++ skare3_tools/scripts/skare3_release_check.py
@@ -153,7 +153,7 @@ def main():
     # this kind of output defines variables 'prerelease' and 'packages' within the workflow.
     print(f'::set-output name=prerelease::{release["prerelease"]}')
     print(f'::set-output name=packages::{packages}')
-    print(f'::set-output name=overwrite_flag::whatever')
+    print(f'::set-output name=overwrite_flag::--skare3-overwrite-version {branch_name}:{tag_name}')
 
 
 if __name__ == '__main__':
