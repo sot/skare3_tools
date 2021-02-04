0cc834ba6ab152f46f35bf8260005c5a62ff3999
Javier Gonzalez <javierggt@yahoo.com>
2021-02-04 16:49:17 -0500

    fixup

--- skare3_tools/conda.py
+++ skare3_tools/conda.py
@@ -3,6 +3,11 @@ import subprocess
 import json
 from pathlib import Path
 import shutil
+import argparse
+import logging
+
+
+logger = logging.getLogger('skare3_tools.conda')
 
 
 def gather_env_pkgs(directory, pkgs_dir=None, include_channels=None, exclude_channels=()):
@@ -16,7 +21,7 @@ def gather_env_pkgs(directory, pkgs_dir=None, include_channels=None, exclude_cha
             else:
                 parts.append(part)
         pkgs_dir = Path(*parts)
-    pkgs = json.loads(subprocess.check_output(['conda', 'list', '--json']).decode())
+    pkgs = json.loads(subprocess.check_output(['conda', 'list', '--no-pip', '--json']).decode())
     pkgs = [p for p in pkgs if p['channel'] not in exclude_channels]
     if include_channels:
         pkgs = [p for p in pkgs if p['channel'] in include_channels]
@@ -28,4 +33,28 @@ def gather_env_pkgs(directory, pkgs_dir=None, include_channels=None, exclude_cha
             name = pkgs_dir / f'{pkg["dist_name"]}.conda'
         elif (pkgs_dir / f'{pkg["dist_name"]}.tar.bz2').exists():
             name = pkgs_dir / f'{pkg["dist_name"]}.tar.bz2'
-        shutil.copy(name, directory)
+        if name:
+            shutil.copy(name, directory)
+        else:
+            logger.debug(f'ignored {pkg["dist_name"]}')
+
+
+def get_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--directory', required=True)
+    parser.add_argument('--pkgs-dir')
+    parser.add_argument('--include-channel', action='append', default=[])
+    parser.add_argument('--exclude-channel', action='append', default=[])
+    return parser
+
+
+def main():
+    args = get_parser().parse_args()
+    gather_env_pkgs(args.directory,
+                    pkgs_dir=args.pkgs_dir,
+                    exclude_channels=args.exclude_channel,
+                    include_channels=args.include_channel)
+
+
+if __name__ == '__main__':
+    main()
