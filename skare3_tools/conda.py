11b827917a87a790f7d1d6aa5fa98de2b89b56e2
Javier Gonzalez <javierggt@yahoo.com>
2021-02-04 21:36:21 -0500

    fix: mkdir(parents=True)

--- skare3_tools/conda.py
+++ skare3_tools/conda.py
@@ -26,7 +26,7 @@ def gather_env_pkgs(directory, pkgs_dir=None, include_channels=None, exclude_cha
     if include_channels:
         pkgs = [p for p in pkgs if p['channel'] in include_channels]
     if pkgs and not directory.exists():
-        directory.mkdir()
+        directory.mkdir(parents=True)
     for pkg in pkgs:
         name = ''
         if (pkgs_dir / f'{pkg["dist_name"]}.conda').exists():
