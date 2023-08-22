cf7004122ea33a7a1896356ec0394c528476ad75
Javier Gonzalez <javierggt@yahoo.com>
2023-08-22 12:05:35 -0400

    in dashboard script, run skare3-test-dashboard without trying to open the browser

--- scripts/update-dashboard.sh
+++ scripts/update-dashboard.sh
@@ -1,8 +1,8 @@
 source $HOME/.ci-auth
 skare3-dashboard -o /proj/sot/ska/jgonzalez/index.html
 skare3-dashboard -o /proj/sot/ska/jgonzalez/packages.json
-skare3-test-dashboard -o /proj/sot/ska/jgonzalez/test_results.html --static-dir https://cxc.cfa.harvard.edu/mta/ASPECT/skare3/dashboard/static --log-dir https://icxc.cfa.harvard.edu/aspect/skare3/dashboard/tests
-skare3-test-dashboard -o /proj/sot/ska/jgonzalez/test_results.json
+skare3-test-dashboard -b -o /proj/sot/ska/jgonzalez/test_results.html --static-dir https://cxc.cfa.harvard.edu/mta/ASPECT/skare3/dashboard/static --log-dir https://icxc.cfa.harvard.edu/aspect/skare3/dashboard/tests
+skare3-test-dashboard -b -o /proj/sot/ska/jgonzalez/test_results.json
 
 # horrible hack:
 rm -f /proj/sot/ska/www/ASPECT_ICXC/skare3/dashboard/tests
